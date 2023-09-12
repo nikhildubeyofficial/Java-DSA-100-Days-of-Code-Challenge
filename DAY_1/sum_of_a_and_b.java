7c8ab2c7e4ade32fbbf74c695963d7a6766848cf
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-13 01:16:38 +0530

    Added new File sum

---
+++ DAY_1/sum_of_a_and_b.java
@@ -0,0 +1,8 @@
+public class sum_of_a_and_b {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
