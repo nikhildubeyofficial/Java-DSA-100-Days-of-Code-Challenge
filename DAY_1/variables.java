e8ead8dcdd31b745c4e7e0de8f85440d88a4ed1f
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-13 01:04:33 +0530

    variables

---
+++ DAY_1/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        // a and b are identifiers
+        String name = "Tony Stark";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
