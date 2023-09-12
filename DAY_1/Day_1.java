9a2aa802df242a77a84a82648433a22e3d9afc7a
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-13 00:42:54 +0530

    Added Folder

---
+++ DAY_1/Day_1.java
@@ -0,0 +1,6 @@
+public class Day_1 {
+    public static void main(String args[]){
+        System.out.println("Hello World!"); // Line ka space next line
+        System.out.print("Hello World!");
+    }
+}
