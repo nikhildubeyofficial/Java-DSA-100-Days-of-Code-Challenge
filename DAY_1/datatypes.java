c03941eec1449c5571cb8cc1983e11e3bf66274f
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-13 01:11:19 +0530

    Added Datatypes

---
+++ DAY_1/datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes {
+    public static void main(String args[]){
+        int a = 4;
+        System.out.println(a);
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        float price = 10.4F;
+        System.out.println(price);
+        // long
+        // double
+        short n = 240;
+        System.out.println(n);
+    }
+}
