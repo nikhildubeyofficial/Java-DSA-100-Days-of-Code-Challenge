23b5ecf8587a2f1f5367f6504a64de4468a6e535
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-13 22:15:19 +0530

    input and comments

---
+++ DAY_1/comments.java
@@ -0,0 +1,15 @@
+public class comments {
+    public static void main(String args[]){
+        // Single line comment
+        // code to calculte sum 
+
+        // Multi line comment
+        /*
+         * adfa
+         * asdff
+         * adsfggh
+         * jhgsdf
+         * asdfhg
+         */
+    }
+}
---
+++ DAY_1/input.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class input {
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+    //    String input = sc.next();
+    //    System.out.println(input); 
+
+    //    String name = sc.nextLine();
+    //    System.out.println(name);
+
+    int number = sc.nextInt();
+    System.out.println(number);
+
+    float price = sc.nextFloat();
+    System.out.println(price);
+
+    // next
+    // nextLine
+    // nextInt
+    // nextByte
+    // nextFloat
+    // nextDouble
+    // nextBoolean
+    // nextShort
+    // nextLong
+
+    
+    }
+}
