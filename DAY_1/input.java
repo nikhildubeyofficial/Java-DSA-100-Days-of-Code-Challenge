f8e8db0f3ff6bb7b843d9e7f9e582fd704603ab1
Nikhil Dubey <115033141+nikhildubeyofficial@users.noreply.github.com>
2023-09-15 14:52:11 +0000

    main

--- DAY_1/input.java
+++ DAY_1/input.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        // String input = sc.next();
-        // System.out.println(input);
+        try (Scanner sc = new Scanner(System.in)) {
+            int number = sc.nextInt();
+            System.out.println(number);
+
+            float price = sc.nextFloat();
+            System.out.println(price);
+        }
 
         // String name = sc.nextLine();
         // System.out.println(name);
 
-        int number = sc.nextInt();
-        System.out.println(number);
-
-        float price = sc.nextFloat();
-        System.out.println(price);
 
         // next
         // nextLine
