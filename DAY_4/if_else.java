6aeb94bbd56f1a52c25a68f5d772263f6e617218
Nikhil Dubey <115033141+nikhildubeyofficial@users.noreply.github.com>
2023-09-16 17:13:13 +0000

    main

---
+++ DAY_4/if_else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class if_else{
+    public static void main(String args[]){
+        int age = 22;
+        if(age >= 18){
+            System.out.println("ADULT");
+        }
+        else{
+            System.out.println("NOT ADULT");
+        }
+    }
+}
