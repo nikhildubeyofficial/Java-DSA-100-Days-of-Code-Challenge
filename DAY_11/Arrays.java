5ac41fbcafc7464561970115a8b74bb82f0fdf18
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-09-30 00:04:03 +0530

    main

---
+++ DAY_11/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays{
+    public static void main(String args[]){
+
+        // Array Creation
+        int marks[] = new int[50];
+        int numbers[] = {1,2,3};
+        int moreNumbers[] = {4,5,6};
+
+        // Input Output
+        
+        
+
+    }
+}
