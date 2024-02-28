207a6293684a73b897b55f418795f8ea762baa67
Nikhil Dubey <officialnikhildubey@gmail.com>
2024-02-28 23:21:59 +0530

    DAY 8

---
+++ DAY_18/insertionSort.java
@@ -0,0 +1,30 @@
+package DAY_18;
+
+public class insertionSort {
+    public static void insertion(int arr[]){
+        for (int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        insertion(arr);
+        printArr(arr);
+    }
+}
